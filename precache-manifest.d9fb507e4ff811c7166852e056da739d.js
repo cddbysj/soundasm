3df5207a9463aee48684cb873f0b71ef46e78dcb
cddbysj <cddbysj@qq.com>
2021-04-27 15:00:20 +0800

    Updates

--- precache-manifest.d9fb507e4ff811c7166852e056da739d.js
+++ precache-manifest.d9fb507e4ff811c7166852e056da739d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "66250fce9a068b305a72b886c46d002b",
+    "revision": "5502506ced34a55213c627a7c26e0fff",
     "url": "/soundasm/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/soundasm/static/css/2.69a21dd3.chunk.css"
   },
   {
-    "revision": "acfc16b750190faa0a72",
-    "url": "/soundasm/static/css/main.531c66cb.chunk.css"
+    "revision": "82d02f4d0cdce4f40044",
+    "url": "/soundasm/static/css/main.b1376634.chunk.css"
   },
   {
     "revision": "dea45230e808457b1d5e",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/soundasm/static/js/2.d0962383.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "acfc16b750190faa0a72",
-    "url": "/soundasm/static/js/main.75b84c3a.chunk.js"
+    "revision": "82d02f4d0cdce4f40044",
+    "url": "/soundasm/static/js/main.9fb5c580.chunk.js"
   },
   {
     "revision": "6a61d4d06bc6d591d76d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/soundasm/precache-manifest.b5b4dabd9e3df8b3d6bfb8496dd36b14.js"
+  "/soundasm/precache-manifest.d9fb507e4ff811c7166852e056da739d.js"
 );
 
 self.addEventListener('message', (event) => {
